5766ec8a7daa8f59cd2e2a4db9316649b626c54d
Edwin <edwin12sv@gmail.com>
2023-12-30 00:18:32 -0600

    Initial Commit

---
+++ MapViews/MapViewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapViewsApp.swift
+//  MapViews
+//
+//  Created by elsalvador on 30/12/23.
+//
+
+import SwiftUI
+
+@main
+struct MapViewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
